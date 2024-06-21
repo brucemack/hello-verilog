54ee61142f1baf3a2fa803d98c14f17dd01dd71c
brucemack <bruce@mackinnon.com>
2024-06-21 08:31:01 -0400

    Added some hiz tests

--- highz/test2.v
+++ highz/test2.v
@@ -1,4 +1,5 @@
 // Demonstration of high-z
+// This is relevant to the DOT-OR logic in the SMS cards.
 module test2;
 
     reg in_0;
@@ -7,10 +8,18 @@ module test2;
     assign dot_0 = (in_0 == 0 || in_1 == 0) ? 0 : 1'bz;
     assign dot_1 = (in_0 === 0 || in_1 === 0) ? 0 : 1'bz;
 
+    // Wires
+    wire W_0000_0000_O;
+    wire W_0000_0001_O;
+    wire W_DOT_1 = (W_0000_0000_O === 0 || W_0000_0001_O === 0) ? 0 : 1'bz;
+
+    assign W_0000_0000_O = in_0;
+    assign W_0000_0001_O = in_1;
+
     initial begin 
 
-        $monitor("MON: 0: [%0t] in0: %0d in1: %0d dot_0: %0d dot_1: %0d", 
-            $time, in_0, in_1, dot_0, dot_1);
+        $monitor("MON: 0: [%0t] in0: %0d in1: %0d dot_0: %0d dot_1: %0d W_DOT_1: %0d", 
+            $time, in_0, in_1, dot_0, dot_1, W_DOT_1);
 
         // Here both inputs are floating.  Notice how dot_0 is indeterminant since
         // the == 0 evaluation can't work.
@@ -27,6 +36,7 @@ module test2;
         #1;
         // Here the output is known
         in_1 = 1;
+
 
         #20 $stop;
     end 
